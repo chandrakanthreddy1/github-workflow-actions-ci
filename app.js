3480d580a4adb47f8492921d0a2fa940b7ba25e7
cpi25 <chandrakanthreddy.pesaladinne@sky.uk>
2026-01-18 22:21:03 +0530

    Learning github actions

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Simple endpoint
+app.get('/', (req, res) => {
+  res.send('Hello World! Version 1');
+});
+
+// Another endpoint to test changes
+app.get('/ping', (req, res) => {
+  res.json({ message: "pong" });
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
